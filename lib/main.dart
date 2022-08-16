9bc62c35a9e8d36c329e408abab94c5c5b70ef79
fazelginanda <fzginanda@gmail.com>
2022-08-16 14:43:19 +0700

    Penambahan screen bagian profile
    Bagian front end: profile

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:milestone/screens/chatWindow_screen.dart';
 import 'package:milestone/screens/chat_list.dart';
 import 'package:milestone/screens/findFriend_screen.dart';
 import 'package:milestone/screens/login_screen.dart';
+import 'package:milestone/screens/profile_screen.dart';
 import 'package:milestone/screens/test_screens/signup_test.dart';
 import 'package:milestone/utils/colors.dart';
 
@@ -50,7 +51,7 @@ class MyApp extends StatelessWidget {
       // ),
 
       //Kalau mau ngerun dan liat hasil screen bisa diganti disini
-      home: SettingsScreen(),
+      home: ProfileScreen(),
     );
   }
 }
